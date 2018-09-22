44f1057cc0e76f9a6021b7b154db201016d9b299
deepakcodefreak <deepak19.engg@gmail.com>
2018-09-23 01:36:33 +0530

    addnewmessage and create new message

--- server/server.js
+++ server/server.js
@@ -1,13 +1,36 @@
 const path = require('path');
+const http = require('http')
 const express = require('express')
+const socketIO = require('socket.io')
+
 const publicPath = path.join(__dirname,'../public')
 const port  = process.env.PORT||3000;
 const app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
+io.on('connection',(socket)=>{
+  console.log('New user connected');
+
+  socket.emit('newMessage',{
+    from:'dcdeepak',
+    text:'i am deepak chawla ----deepakcodefreak'
+  });
+
+socket.on('createMessage',(message)=>{
+console.log('Message',message);
+})
 
 
-app.listen(port,()=>{
+});
+
+io.on('disconnect',()=>{
+    console.log('Disconnect from client');
+  });
+
+
+server.listen(port,()=>{
   console.log(`App is live on port ${port}`);
 })
